59fa1db77e8dbe34e5715bb6a8401fed8e3540ec
LinShihJhang <els910270@gmail.com>
2017-05-02 19:45:35 +0800

    Scan Finish

--- SynFloodTest.py
+++ SynFloodTest.py
@@ -1,4 +1,34 @@
-import socket
-import thread
+import socket  
+import threading 
+import time
 
-aaaaa
+socket.setdefaulttimeout(3)
+
+
+def portTest(ip,port):
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result=s.connect_ex((ip,port))
+    if (result == 0 ):
+        print(str(port)+" 開放")
+    s.close()
+
+
+def portScan(ip):
+    threadsList = []
+    for port in range(1024):
+        thread = threading.Thread(target = portTest, args = (ip, port), name = 'port-'+str(port))
+        thread.start()
+        threadsList.append(thread)
+        time.sleep(0.05)
+
+    for t in threadsList:
+        t.join()
+    print("掃描結束")
+
+portScan("163.13.127.236")
+        
+
+
+
+
+
